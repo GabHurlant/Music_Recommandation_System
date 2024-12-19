3f9f81089f23ddc7b03941e9e4e3ea3067bcce8d
GabHurlant <gabin.lasne@gmail.com>
2024-12-19 08:48:59 +0100

    initial commit, model, dataset, reco.py

---
+++ reco.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import OneHotEncoder
