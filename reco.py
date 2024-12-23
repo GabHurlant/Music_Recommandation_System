a8af25ed5fbcb0c091aa8fa78343a6412ca7ebf1
GabHurlant <gabin.lasne@gmail.com>
2024-12-23 09:38:44 +0100

    test

--- reco.py
+++ reco.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
+#import seaborn as sns
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics.pairwise import cosine_similarity
